e59ad3c6bb7eff157ade1c15ca665a792acdc750
Subham Banerjee <32572976+subban358@users.noreply.github.com>
2021-02-14 19:47:46 +0530

    Update Overlapping Intervals.py

--- Overlapping Intervals.py
+++ Overlapping Intervals.py
@@ -17,3 +17,18 @@ for _ in range(int(input())):
         for j in i:
             print(j,end=" ")        
     print()        
+##############################################################
+
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        
+        intervals.sort(key =  lambda x : x[0])
+        merged = []
+        
+        for interval in intervals:
+            if not merged or merged[-1][1] < interval[0]:
+                merged.append(interval)
+            else:
+                merged[-1][1] = max(merged[-1][1], interval[1])
+        
+        return merged
